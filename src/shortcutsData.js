d9d6f2b0b711a68174b666f4b3d22dcf591bbfc8
Jenny Martin <jennymartin@Jennys-MBP.lan1>
2019-08-02 17:11:25 -0500

    added shortcuts data

--- src/shortcutsData.js
+++ src/shortcutsData.js
@@ -18,6 +18,31 @@ const shortcutsData = [
         id: 4, 
         text: "shift + command + N = create new folder", 
         completed: true
+    }, 
+    {
+        id: 5, 
+        text: "option + command + = sleep", 
+        completed: false
+    }, 
+    {
+        id: 6, 
+        text: "control + command + power = restart",
+        completed: false
+    },
+    {
+        id:7,
+        text:"option + control + command + power = shutdown", 
+        completed: false
+    },
+    {
+        id: 8, 
+        text: "command + up arrow = scroll to top of page",
+        completed: false
+    },
+    {
+        id: 9, 
+        text: "command + down arrow = bottom of page",
+        completed: false
     }
 ]
 
